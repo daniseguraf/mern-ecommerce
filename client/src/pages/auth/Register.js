bb196e1d5a52dffc248bf6c844ac68ab8ad48655
Daniel Segura <danielseguraf@gmail.com>
2022-02-24 22:57:11 -0500

    register form

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
-  return <div>Register</div>;
+  const [value, setValue] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const registerForm = () => (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        className="form-control"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        autoFocus
+      />
+
+      <button type="submit" className="btn btn-raised">
+        Register
+      </button>
+    </form>
+  );
+  return (
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h4>Register</h4>
+          {registerForm()}
+          {value}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
