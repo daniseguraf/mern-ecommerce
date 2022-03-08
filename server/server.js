e14cb2eb4ec5a62b57f0bec9bea4bda926d2daed
Daniel Segura <danielseguraf@gmail.com>
2022-03-07 23:41:02 -0500

    server set up

--- server/server.js
+++ server/server.js
@@ -2,6 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => res.send(`API running on 5050`));
+app.get('/', (req, res) => res.send(`API mern-ecommerce running on 5050`));
 
 app.listen(5050, console.log('Server mern-ecommerce running'));
