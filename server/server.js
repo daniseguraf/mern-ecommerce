df89dab289c94baf9089fab546386a9964acd555
Daniel Segura <danielseguraf@gmail.com>
2022-03-08 00:18:47 -0500

    node env

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5050;
 
-app.get('/', (req, res) => res.send(`API mern-ecommerce running on 5050`));
+app.get('/', (req, res) => res.send(`API mern-ecommerce running on ${PORT}`));
 
-app.listen(5050, console.log('Server mern-ecommerce running'));
+app.listen(PORT, console.log(`Server mern-ecommerce running on ${PORT}`));
